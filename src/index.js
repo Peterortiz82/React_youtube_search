cdbd4bc0845d9fd1fd062c56d608f16ffc8ba14a
peterortiz82 <peterortiz82@gmail.com>
2017-04-22 22:18:34 -0400

    updated src/index.js to use the youtube api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const keys = require('./../keys');
 
-const App = () => {
-  return(
-    <div>
-      <SearchBar/>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: keys.you_tube.api_key, term: 'Reactjs'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
